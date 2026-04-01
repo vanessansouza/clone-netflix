b45da7b1bdd83a9775429d7e39e0c0683efd9a64
Vanessa Souza <vanessansouza21@gmail.com>
2026-04-01 16:08:22 +0900

    update

--- catalogo/js/data.js
+++ catalogo/js/data.js
@@ -23,7 +23,7 @@ export const categories = [
     {
         title: "Séries",
         items: [
-            { img: "https://m.media-amazon.com/images/M/MV5BM2U4NTFmMjktMDNkYy00MDdmLWEzNzktZDQ0MTAxNmY4ZWQ5XkEyXkFqcGc@._V1_.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://youtu.be/DMNjH5MlQXc?si=jGdJdsnFMLGNZRvL" },
+            { img: "https://m.media-amazon.com/images/M/MV5BM2U4NTFmMjktMDNkYy00MDdmLWEzNzktZDQ0MTAxNmY4ZWQ5XkEyXkFqcGc@._V1_.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=DMNjH5MlQXc" },
             { img: "https://m.media-amazon.com/images/M/MV5BMzgwNWE2NGItZmU0NC00ODFiLTg3YjQtNTJmZDM1YjI5YzMzXkEyXkFqcGc@._V1_.jpg", top10: true, youtube: "https://youtu.be/TEgrH93KLgY?si=xxGA1H6h9VNdIS3C" },
             { img: "https://m.media-amazon.com/images/M/MV5BNGZjNWNlZGItZTdiNy00NDNmLWIyNDQtY2UzMjUwMThhMWJhXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", badge: "Novo episódio", badgeColor: "red", youtube: "https://youtu.be/IREMcPoGb5g?si=ARjNF-gBjf5iXDPL" },
             { img: "https://br.web.img3.acsta.net/c_310_420/pictures/20/11/04/11/32/4052345.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://youtu.be/gpv7ayf_tyE?si=9oXDXHilZU8KHm0A" },
@@ -32,7 +32,7 @@ export const categories = [
     {
         title: "Para maratonar",
         items: [
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://m.media-amazon.com/images/M/MV5BMWUwNDFiNjQtYjQ0MC00MTcxLWE0MGQtNTdkYTlhZGU2NDFmXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", top10: true, youtube: "https://youtu.be/BUfSen2rYQs?si=aooe5Nw7tKItazvc" },
             { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
             { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
             { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
