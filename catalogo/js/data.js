67addf8939383e591f047ff13c4a0308725fd852
Vanessa Souza <vanessansouza21@gmail.com>
2026-04-01 16:32:05 +0900

    update data

--- catalogo/js/data.js
+++ catalogo/js/data.js
@@ -7,16 +7,16 @@ export const categories = [
                 youtube: "https://www.youtube.com/watch?v=bLvqoHBptjg"
             },
             {
-                img: "https://aventurasnahistoria.com.br/wp-content/uploads/entretenimento/gladiador_2_VvnGVes.jpg", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=cXg62-t8BWs"
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSReP3wLew2uFtGlz52zCYUSOe1yE-q4GJrWA&s", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=9c-DrMe8o5Q"
             },
             {
                 img: "https://i.ytimg.com/vi/OQgySPQ5M3Y/maxresdefault.jpg", progress: 0,
                 youtube: "https://www.youtube.com/watch?v=zckJCxYxn1g"
             },
             {
-                img: "https://ingresso-a.akamaihd.net/img/cinema/cartaz/14413-destaque.jpg", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=a06zxOyQrAs"
+                img: "https://m.media-amazon.com/images/S/pv-target-images/ebaa5cfc08942df322399efd79caae1256be055082a1478f9e834595b80e6941._SX1080_FMjpg_.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=PnqUs3xiAVI"
             },
         ]
     },
@@ -24,18 +24,19 @@ export const categories = [
         title: "Séries",
         items: [
             { img: "https://m.media-amazon.com/images/M/MV5BM2U4NTFmMjktMDNkYy00MDdmLWEzNzktZDQ0MTAxNmY4ZWQ5XkEyXkFqcGc@._V1_.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=DMNjH5MlQXc" },
-            { img: "https://m.media-amazon.com/images/M/MV5BMzgwNWE2NGItZmU0NC00ODFiLTg3YjQtNTJmZDM1YjI5YzMzXkEyXkFqcGc@._V1_.jpg", top10: true, youtube: "https://youtu.be/TEgrH93KLgY?si=xxGA1H6h9VNdIS3C" },
-            { img: "https://m.media-amazon.com/images/M/MV5BNGZjNWNlZGItZTdiNy00NDNmLWIyNDQtY2UzMjUwMThhMWJhXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", badge: "Novo episódio", badgeColor: "red", youtube: "https://youtu.be/IREMcPoGb5g?si=ARjNF-gBjf5iXDPL" },
-            { img: "https://br.web.img3.acsta.net/c_310_420/pictures/20/11/04/11/32/4052345.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://youtu.be/gpv7ayf_tyE?si=9oXDXHilZU8KHm0A" },
+            { img: "https://m.media-amazon.com/images/M/MV5BMzgwNWE2NGItZmU0NC00ODFiLTg3YjQtNTJmZDM1YjI5YzMzXkEyXkFqcGc@._V1_.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=TEgrH93KLgY" },
+            { img: "https://m.media-amazon.com/images/M/MV5BNGZjNWNlZGItZTdiNy00NDNmLWIyNDQtY2UzMjUwMThhMWJhXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=IREMcPoGb5g" },
+            { img: "https://br.web.img3.acsta.net/c_310_420/pictures/20/11/04/11/32/4052345.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=gpv7ayf_tyE" },
         ]
     },
     {
-        title: "Para maratonar",
+        title: "Animes",
         items: [
-            { img: "https://m.media-amazon.com/images/M/MV5BMWUwNDFiNjQtYjQ0MC00MTcxLWE0MGQtNTdkYTlhZGU2NDFmXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", top10: true, youtube: "https://youtu.be/BUfSen2rYQs?si=aooe5Nw7tKItazvc" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://m.media-amazon.com/images/M/MV5BMzE0ZDU1MzQtNTNlYS00YjNlLWE2ODktZmFmNDYzMTBlZTBmXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=Ud-PtULy4gs" },
+            { img: "https://m.media-amazon.com/images/M/MV5BMWUwNDFiNjQtYjQ0MC00MTcxLWE0MGQtNTdkYTlhZGU2NDFmXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=BUfSen2rYQs" },
+            { img: "https://www.slugmag.com/wp/wp-content/uploads/2017/04/screen-shot-2016-11-27-at-1-42-50-pm.png", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=a2GujJZfXpg&list=RDa2GujJZfXpg&start_radio=1" },
             { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            
         ]
     }
 ];
